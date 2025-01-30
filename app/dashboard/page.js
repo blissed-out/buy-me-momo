15edfebc2f94baed919b6a6771d97a9ed453a5d2
_blissed_ <aishadow317@gmail.com>
2025-01-30 08:49:08 +0545

    router switched to navigation

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,7 +1,6 @@
 "use client";
 import { useSession } from "next-auth/react";
-// import { useRouter } from "next/navigation";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Dashboard = () => {
