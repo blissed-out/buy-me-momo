75d483124caa4c6d84d251e0b216b225e343e281
_blissed_ <aishadow317@gmail.com>
2025-01-30 08:39:27 +0545

    client component fix in dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,16 +1,17 @@
+"use client";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import React from "react";
 
 const Dashboard = () => {
-const { data: session } = useSession();
-const router = useRouter();
+  const { data: session } = useSession();
+  const router = useRouter();
 
-if (!session) {
+  if (!session) {
     router.push("/login");
-}
+  }
 
-return <>dashboard rock</>;
+  return <>dashboard rock</>;
 };
 
 export default Dashboard;
